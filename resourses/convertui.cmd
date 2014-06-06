e20ea5f64b81f3c690dd0cc2705553c548db7bd8
Alexander Gorokhov <sashgorokhov@gmail.com>
2014-06-06 20:13:13 +0600

    Forgot about new files

---
+++ resourses/convertui.cmd
@@ -0,0 +1,4 @@
+C:\Python33\Scripts\pyside-uic.exe loadform.ui -o ..\modules\forms\ui\loadform.py
+C:\Python33\Scripts\pyside-uic.exe mainform.ui -o ..\modules\forms\ui\mainform.py
+C:\Python33\Scripts\pyside-uic.exe audiowidget.ui -o ..\modules\forms\ui\audiowidget.py
+C:\Python33\Scripts\pyside-uic.exe audiodownloadwidget.ui -o ..\modules\forms\ui\audiodownloadwidget.py
